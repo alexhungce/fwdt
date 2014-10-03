294def1343ebb6318f88348c24d909a389a8c58f
Alex Hung <alex.hung@canonical.com>
2014-10-03 11:56:24 +0800

    all: add copyright and update copyright year to 2014
    Signed-off-by: Alex Hung <alex.hung@canonical.com>

--- src/fwdt.c
+++ src/fwdt.c
@@ -1,7 +1,7 @@
 /*
  * FWDT driver
  *
- * Copyright(C) 2012 Canonical Ltd.
+ * Copyright(C) 2014 Canonical Ltd.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
