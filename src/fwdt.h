5d026bea50d598f3330de5fe7ac7559c25194995
Alex Hung <alex.hung@canonical.com>
2018-07-05 15:47:30 -0700

    fwdt: format the h files by clang-format
    Linux Kernel style is used as below:
    
      BasedOnStyle: LLVM
      IndentWidth: 8
      UseTab: Always
      BreakBeforeBraces: Linux
      AllowShortIfStatementsOnASingleLine: false
      IndentCaseLabels: false
    
    see https://clang.llvm.org/docs/ClangFormatStyleOptions.html
    
    Signed-off-by: Alex Hung <alex.hung@canonical.com>

--- src/fwdt.h
+++ src/fwdt.h
@@ -18,105 +18,98 @@
 #define __FWDT_H__
 
 enum fwdt_acpi_vga_sub_cmd {
-	GET_BRIGHTNESS		=	0x01,
-	SET_BRIGHTNESS		=	0x02,
-	GET_BRIGHTNESS_LV	=	0x03,
-	GET_VIDEO_DEVICE	=	0x04,
+	GET_BRIGHTNESS = 0x01,
+	SET_BRIGHTNESS = 0x02,
+	GET_BRIGHTNESS_LV = 0x03,
+	GET_VIDEO_DEVICE = 0x04,
 };
 
 enum fwdt_acpi_ec_sub_cmd {
-	GET_EC_REGISTER		=	0x01,
-	SET_EC_REGISTER		=	0x02,
-	CALL_EC_QMETHOD		=	0x03,
-	CHECK_EC_DEVICE		=	0x04,
+	GET_EC_REGISTER = 0x01,
+	SET_EC_REGISTER = 0x02,
+	CALL_EC_QMETHOD = 0x03,
+	CHECK_EC_DEVICE = 0x04,
 };
 
 enum fwdt_hw_access_sub_cmd {
-	GET_DATA_BYTE		=	0x01,
-	SET_DATA_BYTE		=	0x02,
-	GET_DATA_WORD		=	0x03,
-	SET_DATA_WORD		=	0x04,
-	GET_DATA_DWORD		= 	0x05,
-	SET_DATA_DWORD		= 	0x06,
+	GET_DATA_BYTE = 0x01,
+	SET_DATA_BYTE = 0x02,
+	GET_DATA_WORD = 0x03,
+	SET_DATA_WORD = 0x04,
+	GET_DATA_DWORD = 0x05,
+	SET_DATA_DWORD = 0x06,
 };
 
 typedef struct {
 	u16 func;
-	u16	reserved;
+	u16 reserved;
 } fwdt_parameter;
 
-#define FWDT_SUCCESS		0
-#define FWDT_FAIL		1
+#define FWDT_SUCCESS 0
+#define FWDT_FAIL 1
 
 struct fwdt_brightness {
-	fwdt_parameter	parameters;
-	char		lcd_path[256];
+	fwdt_parameter parameters;
+	char lcd_path[256];
 	union {
-		u32 		brightness_level;
-		u32 		num_of_levels;
+		u32 brightness_level;
+		u32 num_of_levels;
 	};
-	u32             levels[256];
-} __attribute__ ((packed));
+	u32 levels[256];
+} __attribute__((packed));
 
 struct fwdt_io_data {
-	fwdt_parameter	parameters;
-	u16		io_address;
+	fwdt_parameter parameters;
+	u16 io_address;
 	union {
-		u8	io_byte;
-		u16	io_word;
+		u8 io_byte;
+		u16 io_word;
 	};
-} __attribute__ ((packed));
+} __attribute__((packed));
 
 struct fwdt_mem_data {
-	fwdt_parameter	parameters;
-	u64		mem_address;
-	u32		mem_data;
-} __attribute__ ((packed));
+	fwdt_parameter parameters;
+	u64 mem_address;
+	u32 mem_data;
+} __attribute__((packed));
 
 struct fwdt_cmos_data {
-	fwdt_parameter	parameters;
-	u8		cmos_address;
-	u8		cmos_data;
-} __attribute__ ((packed));
+	fwdt_parameter parameters;
+	u8 cmos_address;
+	u8 cmos_data;
+} __attribute__((packed));
 
 struct fwdt_ec_data {
-	fwdt_parameter	parameters;
+	fwdt_parameter parameters;
 	union {
-		u8		address;
-		u8		q_method;
+		u8 address;
+		u8 q_method;
 	};
-	u8		data;
-} __attribute__ ((packed));
+	u8 data;
+} __attribute__((packed));
 
 struct fwdt_acpi_data {
-	fwdt_parameter	parameters;
-	char	acpi_path[256];
-	u32		para1;
-	u32		para2;
-	u32		retVal;
-} __attribute__ ((packed));
+	fwdt_parameter parameters;
+	char acpi_path[256];
+	u32 para1;
+	u32 para2;
+	u32 retVal;
+} __attribute__((packed));
 
 typedef struct {
-	fwdt_parameter	parameters;
+	fwdt_parameter parameters;
 } fwdt_generic;
 
+#define FWDT_ACPI_VGA_CMD _IOWR('p', 0x01, struct fwdt_brightness)
 
-#define FWDT_ACPI_VGA_CMD \
-        _IOWR('p', 0x01, struct fwdt_brightness)
+#define FWDT_HW_ACCESS_IO_CMD _IOWR('p', 0x02, struct fwdt_io_data)
 
-#define FWDT_HW_ACCESS_IO_CMD \
-        _IOWR('p', 0x02, struct fwdt_io_data)
+#define FWDT_HW_ACCESS_MEMORY_CMD _IOWR('p', 0x03, struct fwdt_mem_data)
 
-#define FWDT_HW_ACCESS_MEMORY_CMD \
-        _IOWR('p', 0x03, struct fwdt_mem_data)
+#define FWDT_HW_ACCESS_CMOS_CMD _IOWR('p', 0x04, struct fwdt_cmos_data)
 
-#define FWDT_HW_ACCESS_CMOS_CMD \
-        _IOWR('p', 0x04, struct fwdt_cmos_data)
+#define FWDT_ACPI_EC_CMD _IOWR('p', 0x05, struct fwdt_ec_data)
 
-#define FWDT_ACPI_EC_CMD \
-        _IOWR('p', 0x05, struct fwdt_ec_data)
-
-#define FWDT_ACPI_AML_CMD \
-        _IOWR('p', 0x06, struct fwdt_acpi_data)
+#define FWDT_ACPI_AML_CMD _IOWR('p', 0x06, struct fwdt_acpi_data)
 
 #endif
--- src/fwdtapp.h
+++ src/fwdtapp.h
@@ -17,11 +17,9 @@
 #ifndef __FWDT_APP_H__
 #define __FWDT_APP_H__
 
-
-#define	u8	unsigned char
-#define	u16	unsigned short
-#define	u32	unsigned int
-#define	u64	unsigned long
-
+#define u8 unsigned char
+#define u16 unsigned short
+#define u32 unsigned int
+#define u64 unsigned long
 
 #endif
